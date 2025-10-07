2bf27426102bda5dd464687494d057a9911f11a9
thepackour <thepackour5610@gmail.com>
2025-10-08 02:58:07 +0900

    feat: 게시판 수정 api 추가
    title이 일치하는 Post를 찾아서 content를 수정

--- src/main/java/edu/causwict/restapi/controller/PostController.java
+++ src/main/java/edu/causwict/restapi/controller/PostController.java
@@ -1,11 +1,9 @@
 package edu.causwict.restapi.controller;
 
 import java.util.Map;
+import java.util.NoSuchElementException;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import edu.causwict.restapi.entity.Post;
 import edu.causwict.restapi.service.PostService;
@@ -28,5 +26,18 @@ public class PostController {
 		Post created = postService.create(title, content);
 
 		return created;
+	}
+
+	// 게시글 수정 api
+	@PatchMapping
+	public Post update(@RequestBody Map<String, Object> param) {
+		String title = (String) param.get("title");
+		String content = (String) param.get("content");
+		try {
+			Post updated = postService.update(title, content);
+			return updated;
+		} catch (NoSuchElementException e) {
+			throw e; // 임시적으로 이렇게 처리, 나중에 에러 핸들러 같은거 만들어서 수정할 수도
+		}
 	}
 }
