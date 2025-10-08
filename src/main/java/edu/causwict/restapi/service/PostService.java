d883164d102cafc88e98f15449d25a9913f9ec0d
thepackour <thepackour5610@gmail.com>
2025-10-08 17:30:48 +0900

    refactor: CreateTitleValidator, UpdateTitleValidator 추가

--- src/main/java/edu/causwict/restapi/service/PostService.java
+++ src/main/java/edu/causwict/restapi/service/PostService.java
@@ -15,16 +15,18 @@ import java.util.Optional;
 @Service
 public class PostService {
 
-	private final PostCooldownChecker postCooldownChecker = new MockPostCooldownChecker();
-
 	private final InMemoryPostRepository postRepository;
+
+	private final PostCooldownChecker postCooldownChecker = new MockPostCooldownChecker();
+	private final CreateTitleValidator createTitleValidator = new CreateTitleValidator();
+	private final UpdateTitleValidator updateTitleValidator = new UpdateTitleValidator();
 
 	public PostService(InMemoryPostRepository postRepository) { // 생성자 주입
 		this.postRepository = postRepository;
 	}
 
 	public Post create(String title, String content) {
-		validateTitle(title);
+		createTitleValidator.validateTitle(title, postRepository);
 		if (postCooldownChecker.checkCooldown() == false) throw new TooManyPostsException("Too Many Posts");
 
 		Post post = new Post(null, title, content);
@@ -34,7 +36,7 @@ public class PostService {
 
     public Post update(String title, String content) {
 		Optional<Post> res = postRepository.findByTitle(title);
-		validateTitle(title);
+		updateTitleValidator.validateTitle(title);
 		if (res.isEmpty()) throw new NoSuchElementException("No data found with title: " + title);
 
 		Post post = res.get();
@@ -52,10 +54,24 @@ public class PostService {
 		if (res.isEmpty()) throw new NoSuchElementException("No data found with title: " + title);
 		return List.of(res.get());
 	}
+}
 
-	private void validateTitle(String title) {
-		if (title == null || title.isEmpty()) throw new IllegalArgumentException("title is null or empty");
-		if (postRepository.findByTitle(title).isPresent()) throw new IllegalArgumentException("title already exists");
+class TitleValidation {
+	public void validateTitle(String title) {
 		if (title.codePointCount(0, title.length()) > 30) throw new IllegalArgumentException("title exceeds 30");
+		if (title.isEmpty()) throw new IllegalArgumentException("title is null or empty");
 	}
+}
+
+class CreateTitleValidator extends TitleValidation {
+	public void validateTitle(String title, InMemoryPostRepository postRepository) {
+		if (postRepository.findByTitle(title).isPresent()) throw new IllegalArgumentException("title already exists");
+		super.validateTitle(title);
+	}
+
+	public CreateTitleValidator() {}
+}
+
+class UpdateTitleValidator extends TitleValidation {
+	public UpdateTitleValidator() {}
 }
